c77cd5ea9ff6e5c9dd77b8651ae2666052562e47
Nuckles1 <danyxcharge@gmail.com>
2026-04-11 18:11:38 +0100

    main

--- app/homepage/Hero.tsx
+++ app/homepage/Hero.tsx
@@ -52,7 +52,7 @@ export const Hero = () => {
                   <p className="tracking-wider">define your style</p>
                 </div>
               </div>
-              <div className="mt-10 relative z-50">
+              <div className="mt-10 relative z-50 text-right">
                 <div className="bg-indigo-300 shadow-2xl pt-4 px-6 rounded-xl overflow-hidden">
                   <Image
                     src={outfit1}
